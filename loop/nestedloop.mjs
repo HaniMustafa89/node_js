e58b306197eb745e8c2857386f7510f615bb3b7f
HaniMustafa89 <webdev3492@gmail.com>
2022-11-02 22:18:23 -0700

    nested_loop

---
+++ loop/nestedloop.mjs
@@ -0,0 +1,10 @@
+const products=["shirts","trouser","jacket"];
+const gifts=["pen","keychain","Tie"];
+
+for (let i = 0; i < products.length; i++) {
+    for (let j = 0; j < gifts.length; j++) {
+        console.log(products[i]);
+        
+    }
+    
+}
